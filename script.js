ddf8bf7c26777151bf4d88bec6305ca665e0569f
Vladimir Larionov <narcisiwe@gmail.com>
2020-10-11 22:34:00 +0300

    fix: change variables

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let money = 1000;
-let income = 300;
-let addExpenses = "Интернет, пиТса, Коммуналка, Авианосец, Кокаинум"; 
-let deposit = true; 
-let mission = 3000000;
-let period = 12;
+const money = 1000;
+const income = 300;
+const addExpenses = "Интернет, пиТса, Коммуналка, Авианосец, Кокаинум"; 
+let addexpenses;
+const deposit = true; 
+const mission = 3000000;
+const period = 12;
 
 console.log(money,income,deposit);
 console.log(addExpenses.length);
 console.log('Период равен '+ period +  ' месяцев. Цель заработать ' + mission + ' евро');
-console.log(addExpenses.toLowerCase() && addExpenses.split(', '));
+addexpenses=addExpenses.toLowerCase();
+console.log(addexpenses.split(', '));
 
-let budgetDay = money/30;
+const budgetDay = money/30;
 console.log(budgetDay);
